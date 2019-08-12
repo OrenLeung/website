f3e7cf11d1446b3d797ca5bccb32a9ce81104729
Oren <oren.leung@gmail.com>
2019-08-11 23:52:51 -0400

    LandingPage

--- src/components/landingpage.jsx
+++ src/components/landingpage.jsx
@@ -6,7 +6,30 @@ class Landing extends Component {
 		return (
 			<div style={{ width: '100%', margin: 'auto' }}>
 				<Grid className="landing-grid">
-					<Cell col={12}></Cell>
+					<Cell col={12}>
+						<img
+							src="https://cdn0.iconfinder.com/data/icons/user-pictures/100/malecostume-512.png "
+							alt="avatar"
+							className="avatar-img"
+						/>
+					</Cell>
+					<div className="banner-text">
+						<h1>Full Stack Web Developer</h1>
+
+						<hr />
+						<p>React | TensorFlow | Computer Vision | Flutter | Express.js</p>
+						<div className="social-links">
+							{/* LinkedIn  */}
+							<a href="https://github.com/OrenLeung" target="_blank" rel="noopener noreferrer">
+								<i className="fa fa-linkedin-square" aria-hidden="true" />
+							</a>
+
+							{/* Github  */}
+							<a href="https://www.linkedin.com/in/oren-leung/" target="_blank" rel="noopener noreferrer">
+								<i className="fa fa-github-square" aria-hidden="true" />
+							</a>
+						</div>
+					</div>
 				</Grid>
 			</div>
 		);
